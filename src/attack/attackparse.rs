ba7acd6b6d8758c7c361a2eaf3896161a16df710
Zack Yancey <mail@zackyancey.com>
2020-04-21 21:03:26 -0600

    Tests for attacks

--- src/attack/attackparse.rs
+++ src/attack/attackparse.rs
@@ -10,3 +10,83 @@ impl FromStr for Attack {
         unimplemented!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::AdvState::*;
+    use crate::DamagePart::Dice as D;
+    use crate::DamagePart::Modifier as M;
+    use crate::{Check, Damage};
+
+    #[test]
+    fn inferred_r() {
+        assert_eq!(
+            "+3?2d8-1".parse::<Attack>().unwrap(),
+            Attack {
+                check: Check {
+                    adv: Neutral,
+                    modifier: Damage(vec![M(3)])
+                },
+                damage: Damage(vec![D(2, 8), M(-1)])
+            }
+        )
+    }
+
+    #[test]
+    fn advantage() {
+        assert_eq!(
+            "a-1?2d8+1".parse::<Attack>().unwrap(),
+            Attack {
+                check: Check {
+                    adv: Advantage,
+                    modifier: Damage(vec![M(-1)])
+                },
+                damage: Damage(vec![D(2, 8), M(1)])
+            }
+        )
+    }
+
+    #[test]
+    fn neutral() {
+        assert_eq!(
+            "r+8?3d10+2".parse::<Attack>().unwrap(),
+            Attack {
+                check: Check {
+                    adv: Neutral,
+                    modifier: Damage(vec![M(8)])
+                },
+                damage: Damage(vec![D(3, 10), M(2)])
+            }
+        )
+    }
+
+    #[test]
+    fn complex() {
+        assert_eq!(
+            "d+1d4+3-1?1d4+4d6+2-1d4".parse::<Attack>().unwrap(),
+            Attack {
+                check: Check {
+                    adv: Disadvantage,
+                    modifier: Damage(vec![D(1, 4), M(3), M(-1)])
+                },
+                damage: Damage(vec![D(1, 4), D(4, 6), M(2), D(1, -4)])
+            }
+        )
+    }
+
+    #[test]
+    fn invalid() {
+        assert!("r+3".parse::<Attack>().is_err());
+        assert!("2d8".parse::<Attack>().is_err());
+        assert!("".parse::<Attack>().is_err());
+        assert!("2d8?3d6".parse::<Attack>().is_err());
+        assert!("r+3?r-2".parse::<Attack>().is_err());
+        assert!("?".parse::<Attack>().is_err());
+        assert!("not?2d10".parse::<Attack>().is_err());
+        assert!("d+3?3d6+4?3d8".parse::<Attack>().is_err());
+        assert!("d+3??3d8".parse::<Attack>().is_err());
+        assert!("d+3??majordamage".parse::<Attack>().is_err());
+    }
+}
--- src/attack/attackroll.rs
+++ src/attack/attackroll.rs
@@ -3,8 +3,14 @@ use std::fmt;
 use crate::{CheckRoll, DamageRoll};
 
 pub struct AttackRoll {
-    pub check: CheckRoll,
-    pub damage: DamageRoll,
+    check: CheckRoll,
+    damage: DamageRoll,
+}
+
+impl AttackRoll {
+    pub fn new(check: CheckRoll, damage: DamageRoll) -> AttackRoll {
+        unimplemented!()
+    }
 }
 
 impl fmt::Display for AttackRoll {
@@ -16,5 +22,57 @@ impl fmt::Display for AttackRoll {
 impl fmt::Debug for AttackRoll {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::AdvState::*;
+    use crate::DamageRollPart::Dice as Dr;
+    use crate::DamageRollPart::Modifier as Mr;
+
+    #[test]
+    fn no_modifier() {
+        let r = AttackRoll::new(
+            CheckRoll::new(&Neutral, 10, 16, DamageRoll::new(vec![])),
+            DamageRoll::new(vec![Dr(6, vec![5, 4]), Mr(4)]),
+        );
+
+        assert_eq!(format!("{}", r), "10 ? 13");
+        assert_eq!(format!("{:?}", r), "(10) ? [5+4]+4");
+    }
+
+    #[test]
+    fn with_modifier() {
+        let r = AttackRoll::new(
+            CheckRoll::new(&Advantage, 5, 12, DamageRoll::new(vec![Mr(3)])),
+            DamageRoll::new(vec![Dr(8, vec![2, 6, 8]), Mr(-2)]),
+        );
+
+        assert_eq!(format!("{}", r), "15 ? 14");
+        assert_eq!(format!("{:?}", r), "(5/12)+3 ? [2+6+8]-2");
+    }
+
+    #[test]
+    fn critical() {
+        let r = AttackRoll::new(
+            CheckRoll::new(&Advantage, 20, 4, DamageRoll::new(vec![Mr(3)])),
+            DamageRoll::new(vec![Dr(8, vec![2, 6, 8]), Dr(8, vec![1, 5, 2]), Mr(-2)]),
+        );
+
+        assert_eq!(format!("{}", r), "Critical ? 22");
+        assert_eq!(format!("{:?}", r), "(20/4)+3 ? [2+6+8]+[1+5+2]-2");
+    }
+
+    #[test]
+    fn critfail() {
+        let r = AttackRoll::new(
+            CheckRoll::new(&Disadvantage, 15, 1, DamageRoll::new(vec![Mr(3)])),
+            DamageRoll::new(vec![Dr(8, vec![2, 6, 8]), Dr(8, vec![1, 5, 2]), Mr(-2)]),
+        );
+
+        assert_eq!(format!("{}", r), "Fail ? 2");
+        assert_eq!(format!("{:?}", r), "(1/15)+3 ? [3+1]-2");
     }
 }
