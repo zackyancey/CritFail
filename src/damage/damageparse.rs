b11b07121e5153fc6360cccb3e51985f72897ab5
Zack Yancey <mail@zackyancey.com>
2020-04-21 21:03:31 -0600

    Convert tests to use a macro

--- src/damage/damageparse.rs
+++ src/damage/damageparse.rs
@@ -67,51 +67,44 @@ impl FromStr for Damage {
 mod tests {
     use super::*;
 
+    macro_rules! test_damagepart {
+        ($string:expr, $damage:expr) => {
+            let d = $string.parse::<DamagePart>().unwrap();
+            assert_eq!(d, $damage);
+        }
+    }
+
+    macro_rules! test_damage {
+        ($string:expr, $damage:expr) => {
+            let d = $string.parse::<Damage>().unwrap();
+            assert_eq!(d, $damage);
+        }
+    }
+
     mod parse_damagepart {
         use super::*;
         #[test]
         fn simple_damage() {
-            let d: DamagePart = "2d8".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(2, 8));
-
-            let d: DamagePart = "1d12".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(1, 12));
-
-            let d: DamagePart = "3d6".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(3, 6));
-
-            let d: DamagePart = "421d314159".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(421, 314159));
+            test_damagepart!("2d8", DamagePart::Dice(2, 8));
+            test_damagepart!("1d12", DamagePart::Dice(1, 12));
+            test_damagepart!("3d6", DamagePart::Dice(3, 6));
+            test_damagepart!("421d314159", DamagePart::Dice(421, 314159));
         }
 
         #[test]
         fn negative_damage() {
-            let d: DamagePart = "-2d8".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(2, -8));
-
-            let d: DamagePart = "-1d12".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(1, -12));
-
-            let d: DamagePart = "-3d6".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(3, -6));
-
-            let d: DamagePart = "-421d314159".parse().unwrap();
-            assert_eq!(d, DamagePart::Dice(421, -314159));
+            test_damagepart!("-2d8", DamagePart::Dice(2, -8));
+            test_damagepart!("-1d12", DamagePart::Dice(1, -12));
+            test_damagepart!("-3d6", DamagePart::Dice(3, -6));
+            test_damagepart!("-421d314159", DamagePart::Dice(421, -314159));
         }
 
         #[test]
         fn modifier() {
-            let d: DamagePart = "4".parse().unwrap();
-            assert_eq!(d, DamagePart::Modifier(4));
-
-            let d: DamagePart = "-3".parse().unwrap();
-            assert_eq!(d, DamagePart::Modifier(-3));
-
-            let d: DamagePart = "26".parse().unwrap();
-            assert_eq!(d, DamagePart::Modifier(26));
-
-            let d: DamagePart = "-129".parse().unwrap();
-            assert_eq!(d, DamagePart::Modifier(-129));
+            test_damagepart!("4", DamagePart::Modifier(4));
+            test_damagepart!("-3", DamagePart::Modifier(-3));
+            test_damagepart!("26", DamagePart::Modifier(26));
+            test_damagepart!("-129", DamagePart::Modifier(-129));
         }
 
         #[test]
@@ -134,17 +127,10 @@ mod tests {
 
         #[test]
         fn normal_damage() {
-            let d: Damage = "2d6+3".parse().unwrap();
-            assert_eq!(d, Damage(vec![D(2, 6), M(3)]));
-
-            let d: Damage = "8d4-4".parse().unwrap();
-            assert_eq!(d, Damage(vec![D(8, 4), M(-4)]));
-
-            let d: Damage = "-2d8+3".parse().unwrap();
-            assert_eq!(d, Damage(vec![D(2, -8), M(3)]));
-
-            let d: Damage = "3d12+3d6-1d4+2-3".parse().unwrap();
-            assert_eq!(d, Damage(vec![D(3, 12), D(3, 6), D(1, -4), M(2), M(-3)]));
+           test_damage!("2d6+3", Damage(vec![D(2, 6), M(3)]));
+           test_damage!("8d4-4", Damage(vec![D(8, 4), M(-4)]));
+           test_damage!("-2d8+3", Damage(vec![D(2, -8), M(3)]));
+           test_damage!("3d12+3d6-1d4+2-3", Damage(vec![D(3, 12), D(3, 6), D(1, -4), M(2), M(-3)]));
         }
 
         #[test]
